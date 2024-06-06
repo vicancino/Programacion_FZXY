810c021f7ed83e736c59be1eaa4a3fa5cd60b301
MateoTaito <mtaito@alumnos.uai.cl>
2024-06-06 15:44:23 -0400

    Iniciación swagger

---
+++ server/src/doc/swagger.ts
@@ -0,0 +1,28 @@
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+//Metadata info about our API
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: { title: "FZ_XYZ", version: "1.0.0"},
+    },
+    apis: ["src/routes/router.auth.ts", "src/controllers/AuthControllers.ts"],
+};
+
+// Docs en JSON format
+
+const swaggerSpec = swaggerJSDoc(options);
+
+const swaggerDocs = (app, port) => {
+    console.log("proceso de documentacion iniciado");
+    app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    app.get('/api/v1/docs.json', (req, res) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(swaggerSpec);
+});
+
+    console.log('Version 1 Docs are available at http://localhost:4000/api/v1/docs');
+};
+
+module.exports = { swaggerDocs };
