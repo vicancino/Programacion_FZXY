74b9616ff5c6caf2a40dcc414b0d5eaaafa8de70
Vittorio <vicancino@alumnos.uai.cl>
2024-06-17 23:25:13 -0400

    Asistencia Cross

--- client/src/components/pages/asist/Asistencia.tsx
+++ client/src/components/pages/asist/Asistencia.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useQuery } from "react-query";
 import { getUsers } from "../../../api/AsistApi";
-import { ReactHTML, useState } from "react";
+import { ReactHTML, useEffect, useState } from "react";
 
 export default function Entrada() {
 	// Informacion de los usuarios registrados
@@ -11,43 +11,90 @@ export default function Entrada() {
 	});
 
 	// Lista de usuarios registrados
-	const lista_usuarios = data.map((items: [string, string], index: number) => <li key={index}>{items[0]}</li>);
 
-	const usersNames_list = data.map((items: [string, string]) => items[0]);
-	const userEmails_list = data.map((items: [string, string]) => items[1]);
+	const users_Names = data.map((items: [string, string]) => items[0]);
+	const user_Emails = data.map((items: [string, string]) => items[1]);
 
-	const [users, setUsers] = useState(usersNames_list);
-	const [usersE, setUsersE] = useState(userEmails_list);
+	// Input
+	// User
+	const [findUser, setfindUser] = useState("");
 
-	// Variable para registrar asistencia de un nuevo usuario
-	const [NombreUsuario, setNombreUsuario] = useState("");
-	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setNombreUsuario(e.target.value);
-		console.log(NombreUsuario);
-		if (NombreUsuario === "") {
-			setUsers(usersNames_list);
-			return;
-		}
+	// Email
+	const [findEmail, setfindEmail] = useState("");
 
-		const filteredUsers = usersNames_list.filter((user: string) =>
-			user.toLowerCase().includes(NombreUsuario.toLocaleLowerCase())
+	// Bottones Listas
+	// Usuarios
+	const handleNameList = (name: string) => {
+		setfindUser(name);
+		const matching_users = data.filter((items: [string, string]) =>
+			items[0].toLocaleLowerCase().includes(name.toLocaleLowerCase())
 		);
-		setUsers(filteredUsers);
-
-		console.log(users);
+		const matching_emails = matching_users.map((items: [string, string]) => items[1]);
+		// Seleccionamos el primero
+		setfindEmail(matching_emails[0]);
 	};
+
+	// Emails
+	const handleEmailList = (email: string) => {
+		setfindEmail(email);
+		const matching_users = data.filter((items: [string, string]) =>
+			items[1].toLocaleLowerCase().includes(email.toLocaleLowerCase())
+		);
+		const matching_emails = matching_users.map((items: [string, string]) => items[0]);
+		// Seleccionamos el primero
+		setfindUser(matching_emails[0]);
+	};
+
+	// Generacion lista filtrada
+	// Logica Usuarios
+	const filtered_name_list = users_Names.filter((user: string) =>
+		user.toLocaleLowerCase().includes(findUser.toLocaleLowerCase())
+	);
+
+	// Logica Email
+	const filtered_email_list = user_Emails.filter((email: string) =>
+		email.toLocaleLowerCase().includes(findEmail.toLocaleLowerCase())
+	);
+
+	// Vista
+	// Usuarios
+	const button_names_list = filtered_name_list.map((item: string, index: number) => (
+		<li key={index}>
+			<button onClick={() => handleNameList(item)}>{item}</button>
+		</li>
+	));
+	// Emails
+	const button_emails_list = filtered_email_list.map((item: string, index: number) => (
+		<li key={index}>
+			<button onClick={() => handleEmailList(item)}>{item}</button>
+		</li>
+	));
 
 	return (
 		<>
 			<div>Bienvenido al Registro de Asistencia</div>
 			<div>Marcar Asistencia</div>
 			<div>
-				<input type="text" placeholder="Buscar usuarios" value={NombreUsuario} onChange={handleInputChange} />
-				<button>Marcar</button>
+				<input
+					type="text"
+					value={findUser}
+					onChange={(e) => setfindUser(e.target.value)}
+					placeholder="Ingresar Nombre"
+				/>
+
+				{button_names_list}
 			</div>
-			<div>Lista de Usuarios Presentes</div>
-			<div>Lista de Usuarios</div>
-			{lista_usuarios}
+			<div>
+				<input
+					type="text"
+					value={findEmail}
+					onChange={(e) => setfindEmail(e.target.value)}
+					placeholder="Ingresar Correo"
+				/>
+				<button>Marcar</button>
+				{button_emails_list}
+			</div>
+
 			<nav>
 				<Link to={"/nuevo-registro-usuario"}>Registrar nuevo Usuario</Link>
 			</nav>
